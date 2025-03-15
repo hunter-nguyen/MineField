701c2cb373c1d3e82f1d6ba8fc113d04864b2bbf
hunter-nguyen <hunternguyen8@gmail.com>
2025-03-15 13:11:11 -0700

    delete app.java

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import minefield.MineFieldFactory;
-import mvc.AppPanel;
-
-public class App {
-    public static void main(String[] args) {
-        AppPanel panel = new AppPanel(new MineFieldFactory());
-        panel.display();
-    }
-}
